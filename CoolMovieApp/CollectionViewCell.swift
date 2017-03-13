dfc8f90fcd8313ba057d45b919be440f764e56d7
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-03-13 09:29:24 -0400

    Added second VC

--- CoolMovieApp/CollectionViewCell.swift
+++ CoolMovieApp/CollectionViewCell.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    var store = MovieDataStore.sharedInstance
     
     var movie: Movie? {
         didSet {
             
+            guard let urlString = self.movie?.posterURL else { return }
+            posterImg.image = nil
+            
+            if let imageFromCache = store.imageCache.object(forKey: urlString as AnyObject) {
+                movie?.posterImg = (imageFromCache as! UIImage)
+                posterImg.image = (imageFromCache as! UIImage)
+                print()
+                return
+            }
+            
+            guard let url = URL(string: urlString) else { return }
+            guard let data = NSData(contentsOf: url) else { return }
+            guard let image = UIImage(data: data as Data) else { return }
+            
             DispatchQueue.main.async {
-                guard let urlString = self.movie?.posterURL else { return }
-                guard let url = URL(string: urlString) else { return }
-                guard let data = NSData(contentsOf: url) else { return }
-                guard let image = UIImage(data: data as Data) else { return }
-                self.movie?.posterImg = image
+                let imageToCache = image
+                self.store.imageCache.setObject(imageToCache, forKey: urlString as AnyObject)
+                self.movie?.posterImg = imageToCache
                 self.posterImg.image = self.movie?.posterImg
                 print("image set!")
             }
@@ -28,6 +41,20 @@ class CollectionViewCell: UICollectionViewCell {
             
         }
     }
+//            DispatchQueue.main.async {
+//                guard let urlString = self.movie?.posterURL else { return }
+//                guard let url = URL(string: urlString) else { return }
+//                guard let data = NSData(contentsOf: url) else { return }
+//                guard let image = UIImage(data: data as Data) else { return }
+//                self.movie?.posterImg = image
+//                self.posterImg.image = self.movie?.posterImg
+//                print("image set!")
+//            }
+//            
+//            
+//            
+//        }
+//    }
     
     @IBOutlet weak var posterImg: UIImageView!
     
